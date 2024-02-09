2c6483ab00fab62b1bc5a98726dfda972b6544c8
Ivan de Jesus Franco Lima <ivanfrancolima@hotmail.com>
2024-02-09 20:17:57 -0300

    Create condições.js

---
+++ ex008/condições.js
@@ -0,0 +1,12 @@
+var idade = require
+
+if(idade<16){
+    console.log('Não pode votar')
+}else if(idade <18){
+    console.log('Voto Opcional')
+}else if(idade <65){
+    console.log('Voto Obrigatório')
+}else{
+    console.log('Voto Opcional')
+}
+
