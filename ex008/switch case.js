7ae24402ceaa42ae2753d5f1024ecb0a9d4cee2d
Ivan de Jesus Franco Lima <ivanfrancolima@hotmail.com>
2024-02-10 13:49:49 -0300

    switch case

---
+++ ex008/switch case.js
@@ -0,0 +1,23 @@
+var dia = new Date().getDay()
+
+switch (dia){
+    case 1:
+        console.log('Hoje é Segunda-Feira')
+        break
+    case 2:
+        console.log('Hoje é Terça-Feira')
+        break
+    case 3:
+        console.log('Hoje é Quarta-Feira')
+        break
+    case 4:
+        console.log(`Hoje é Quinta Feira`)
+        break
+    case 5:
+        console.log(`Hoje é Sexta-Feira`)
+    case 6:
+        console.log(`Hoje é Sabádo`)
+        break
+    case 7:
+        console.log(`Hoje é Domingo`)
+}
