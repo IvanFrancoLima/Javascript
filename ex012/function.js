f928378e07252923edc93463ac81297d32e4d187
Ivan de Jesus Franco Lima <ivanfrancolima@hotmail.com>
2024-02-14 14:55:35 -0300

    funções normais e recursivas

---
+++ ex012/function.js
@@ -0,0 +1,11 @@
+function parimpa(num){
+    if(num%2==0){
+        return 'par'
+    }else{
+        return 'impar'
+    }
+}
+
+var result = parimpa(5)
+console.log(`O resultado é ${result}!`)
+
---
+++ ex012/soma.js
@@ -0,0 +1,6 @@
+function soma(n1=0,n2=0){ //Se algum dos parâmetros estiverem vazios eles recebem 0
+    return n1 + n2
+}
+
+var result = soma(7,7)
+console.log(`O resultado da soma entre os valores é ${result}`)
