b4e4d0ce7dd2218e66f638942668cd04765275c8
Ivan de Jesus Franco Lima <ivanfrancolima@hotmail.com>
2024-03-20 16:02:27 -0300

    Create switch_case.js

---
+++ ex009/switch_case.js
@@ -0,0 +1,15 @@
+var escolha = 4
+
+switch(escolha){
+    case 1:
+        console.log('Meu braço')
+        break
+    case 2:
+        console.log('Minhas pernas')
+        break
+    case 3:
+        console.log('Meu joelho')
+        break
+    default:
+        console.log('Ok ok, MEU PAUUUUU!')
+}
