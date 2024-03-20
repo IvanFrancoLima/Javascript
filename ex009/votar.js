16749fb4e7bc0242aeadd41c5dd70823b2ed2e23
Ivan de Jesus Franco Lima <ivanfrancolima@hotmail.com>
2024-03-20 10:24:57 -0300

    Update votar.js

---
+++ ex009/votar.js
@@ -0,0 +1,11 @@
+var idade = 65
+
+if(idade<16){
+    console.log('Você ainda não está apto para votar')
+}else if(idade<18){
+    console.log('Você já tem a opção de votar')
+}else if(idade<64){
+    console.log('Você tem a obrigação de votar')
+}else{
+    console.log('Você tem a opção de votar')
+}
