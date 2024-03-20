e9af9ae864bd4b8514b6d0dba13e141ebcef565a
Ivan de Jesus Franco Lima <ivanfrancolima@hotmail.com>
2024-03-20 10:19:44 -0300

    condições

---
+++ ex006/condition.js
@@ -0,0 +1,9 @@
+console.log('Saiba se foi multado')
+
+var velocidade = Number(65.50)
+
+if(velocidade>60){
+    console.log(`Velocidade de ${velocidade} km por hora,FOI MULTADO!!`)
+}else{
+    console.log(`Velocidade de ${velocidade} km por hora,tudo certo!`)
+}
