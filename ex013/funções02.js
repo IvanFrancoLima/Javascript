ffb4e395f168df815db9846fe185c6828dd90649
Ivan de Jesus Franco Lima <ivanfrancolima@hotmail.com>
2024-03-26 17:38:02 -0300

    funções

---
+++ ex013/funções02.js
@@ -0,0 +1,5 @@
+function soma(n1=0,n2){ //Se não for passado algum parâmetro ou referência para n1 ele recebe 0
+    return n1 + n2
+}
+
+console.log(soma(2,7))
---
+++ ex013/funções03.js
@@ -0,0 +1,5 @@
+let quadrado = function(num){
+    return num**2
+}
+
+console.log(quadrado(9))
